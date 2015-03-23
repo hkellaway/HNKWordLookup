6bf7bee4ed29f433e78f6b0e2349c4b2c72f9379
H Kellaway <hkellaway.tech@gmail.com>
2015-03-22 19:05:09 -0700

    Added demo project

---
+++ Example/HNKWordLookup/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HNKWordLookup
+//
+//  Created by Harlan Kellaway on 3/22/15.
+//  Copyright (c) 2015 Harlan Kellaway. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
