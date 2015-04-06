14f13231e9c74b910dad01e3620896f0215958c9
H Kellaway <hkellaway.tech@gmail.com>
2015-04-05 22:07:55 -0700

    Created Definitions VC

---
+++ Demo/HNKWordLookup-Demo/HNKDefinitionsViewController.m
@@ -0,0 +1,22 @@
+//
+//  HNKDefinitionsViewController.m
+//  HNKWordLookup-Demo
+//
+//  Created by Harlan Kellaway on 4/5/15.
+//  Copyright (c) 2015 Harlan Kellaway. All rights reserved.
+//
+
+#import "HNKDefinitionsViewController.h"
+
+@interface HNKDefinitionsViewController ()
+
+@end
+
+@implementation HNKDefinitionsViewController
+
+- (void)viewDidLoad
+{
+  [super viewDidLoad];
+}
+
+@end
