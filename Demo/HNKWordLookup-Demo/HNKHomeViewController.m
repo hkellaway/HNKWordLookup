60203c8ebd6ba25c2bb12524e804bad3673c111f
H Kellaway <hkellaway.tech@gmail.com>
2015-04-05 21:57:26 -0700

    Implemented Word of the Day lookup on button tap

--- Demo/HNKWordLookup-Demo/HNKHomeViewController.m
+++ Demo/HNKWordLookup-Demo/HNKHomeViewController.m
@@ -8,8 +8,16 @@
 
 #import "HNKHomeViewController.h"
 
+#import <HNKWordLookup/HNKWordLookup.h>
+
+#warning Replace YOUR_API_KEY with your API key
+static NSString *const kHNKApiKey = @"YOUR_API_KEY";
+static NSString *const kHNKSegueShowDefinitionsForWordOfTheDay =
+    @"HNKSegueShowDefinitionsForWordOfTheDay";
+
 @interface HNKHomeViewController ()
 
+@property (nonatomic, strong) HNKLookup *lookup;
 @property (weak, nonatomic) IBOutlet UITextField *definitionsTextField;
 @property (weak, nonatomic) IBOutlet UITextField *pronunciationsTextField;
 
@@ -20,6 +28,22 @@
 - (void)viewDidLoad
 {
   [super viewDidLoad];
+
+  [HNKLookup sharedInstanceWithAPIKey:kHNKApiKey];
+  self.lookup = [HNKLookup sharedInstance];
+}
+
+#pragma mark - Navigation
+
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+  if ([segue.identifier
+          isEqualToString:kHNKSegueShowDefinitionsForWordOfTheDay]) {
+    [self.lookup wordOfTheDayWithCompletion:^(HNKWordOfTheDay *wordOfTheDay,
+                                              NSError *error) {
+      NSLog(@"Word of the Day: %@", wordOfTheDay.word);
+    }];
+  }
 }
 
 @end
