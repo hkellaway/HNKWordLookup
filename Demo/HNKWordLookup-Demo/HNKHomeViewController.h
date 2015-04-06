53913aea9a15f7e90029541a5af5fb0eb58012d0
H Kellaway <hkellaway.tech@gmail.com>
2015-04-05 18:41:04 -0700

    Created Home VC

---
+++ Demo/HNKWordLookup-Demo/HNKHomeViewController.h
@@ -0,0 +1,13 @@
+//
+//  HNKHomeViewController.h
+//  HNKWordLookup-Demo
+//
+//  Created by Harlan Kellaway on 4/5/15.
+//  Copyright (c) 2015 Harlan Kellaway. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HNKHomeViewController : UIViewController
+
+@end
---
+++ Demo/HNKWordLookup-Demo/HNKHomeViewController.m
@@ -0,0 +1,22 @@
+//
+//  HNKHomeViewController.m
+//  HNKWordLookup-Demo
+//
+//  Created by Harlan Kellaway on 4/5/15.
+//  Copyright (c) 2015 Harlan Kellaway. All rights reserved.
+//
+
+#import "HNKHomeViewController.h"
+
+@interface HNKHomeViewController ()
+
+@end
+
+@implementation HNKHomeViewController
+
+- (void)viewDidLoad
+{
+  [super viewDidLoad];
+}
+
+@end
