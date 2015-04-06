fb83d40bc7664bf04ad87d4a1e278821c29ffef2
H Kellaway <hkellaway.tech@gmail.com>
2015-04-05 18:26:34 -0700

    Configured demo project

--- Demo/HNKWordLookup-Demo/HNKAppDelegate.h
+++ Demo/HNKWordLookup-Demo/HNKAppDelegate.h
@@ -8,10 +8,8 @@
 
 #import <UIKit/UIKit.h>
 
-@interface AppDelegate : UIResponder <UIApplicationDelegate>
+@interface HNKAppDelegate : UIResponder <UIApplicationDelegate>
 
 @property (strong, nonatomic) UIWindow *window;
 
-
 @end
-
