01871cf6013bc3b80e53720986410dea8e88a921
H Kellaway <hkellaway.tech@gmail.com>
2015-04-05 20:58:45 -0700

    Added IB outlets for Home VC

--- Demo/HNKWordLookup-Demo/HNKHomeViewController.m
+++ Demo/HNKWordLookup-Demo/HNKHomeViewController.m
@@ -10,6 +10,9 @@
 
 @interface HNKHomeViewController ()
 
+@property (weak, nonatomic) IBOutlet UITextField *definitionsTextField;
+@property (weak, nonatomic) IBOutlet UITextField *pronunciationsTextField;
+
 @end
 
 @implementation HNKHomeViewController
@@ -17,6 +20,28 @@
 - (void)viewDidLoad
 {
   [super viewDidLoad];
+}
+
+#pragma mark - IB Actions
+
+- (IBAction)definitionsButtonTapped:(id)sender
+{
+  NSLog(@"Definitions Button Tapped");
+}
+
+- (IBAction)pronunciationsButtonTapped:(id)sender
+{
+  NSLog(@"Pronunciations Button Tapped");
+}
+
+- (IBAction)wordOfTheDayButtonTapped:(id)sender
+{
+  NSLog(@"Word of the Day Button Tapped");
+}
+
+- (IBAction)randomWordTapped:(id)sender
+{
+  NSLog(@"Random Word Tapped");
 }
 
 @end
