ba09bddd44a7e0472b2dec35f93503821a613f86
H Kellaway <hkellaway.tech@gmail.com>
2015-04-07 21:22:11 -0700

    Made better check for if Word of the Day is today

---
+++ Demo/HNKWordLookup-Demo/NSDate+HNKDAdditions.h
@@ -0,0 +1,15 @@
+//
+//  NSDate+HNKDAdditions.h
+//  HNKWordLookup-Demo
+//
+//  Created by Harlan Kellaway on 4/7/15.
+//  Copyright (c) 2015 Harlan Kellaway. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSDate (HNKDAdditions)
+
+- (BOOL)isToday;
+
+@end
