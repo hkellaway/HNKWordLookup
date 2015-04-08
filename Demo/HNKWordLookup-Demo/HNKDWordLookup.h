79b2b863a53daa92b25721699f984be9eb59d60f
H Kellaway <hkellaway.tech@gmail.com>
2015-04-07 23:34:50 -0700

    Refactored word lookup

---
+++ Demo/HNKWordLookup-Demo/HNKDWordLookup.h
@@ -0,0 +1,18 @@
+//
+//  HNKDWordLookup.h
+//  HNKWordLookup-Demo
+//
+//  Created by Harlan Kellaway on 4/7/15.
+//  Copyright (c) 2015 Harlan Kellaway. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HNKDWordLookup : NSObject
+
+- (void)fetchRandomWordWithCompletion:(void (^)(NSString *,
+                                                NSError *))completion;
+- (void)fetchWordOfTheDayWithCompletion:(void (^)(NSString *,
+                                                  NSError *))completion;
+
+@end
