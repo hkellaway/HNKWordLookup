e81ff6ce109ed19d6e9fd1fc1ba8c359b7916b44
H Kellaway <hkellaway.tech@gmail.com>
2015-04-08 00:03:15 -0700

    Created Pronunciations VC

---
+++ Demo/HNKWordLookup-Demo/HNKDPronunciationsViewController.h
@@ -0,0 +1,15 @@
+//
+//  HNKDPronunciationsViewController.h
+//  HNKWordLookup-Demo
+//
+//  Created by Harlan Kellaway on 4/7/15.
+//  Copyright (c) 2015 Harlan Kellaway. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HNKDPronunciationsViewController : UIViewController
+
+@property (nonatomic, copy) NSString *word;
+
+@end
